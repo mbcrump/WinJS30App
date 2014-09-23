7a2ce66aada2d38241a349e113f8f1f89c6dc803
Michael <mbcrump29@gmail.com>
2014-09-23 14:46:06 -0500

    fix

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 var array = [
-    { type: "item", title: "Crump", picture: "http://developer.telerik.com/wp-content/uploads/userphoto/71.png" },
-    { type: "item", title: "Fritz", picture: "http://developer.telerik.com/wp-content/uploads/userphoto/61.jpeg" },
-    { type: "item", title: "Basu", picture: "http://developer.telerik.com/wp-content/uploads/userphoto/161.jpeg" }
+    { type: "item", title: "Michael Crump", picture: "http://developer.telerik.com/wp-content/uploads/userphoto/71.png" },
+    { type: "item", title: "Burke Holland", picture: "http://developer.telerik.com/wp-content/uploads/userphoto/1.png" },
+    { type: "item", title: "Jeff Fritz", picture: "http://developer.telerik.com/wp-content/uploads/userphoto/61.jpeg" },
+    { type: "item", title: "Brian Rinaldi", picture: "http://developer.telerik.com/wp-content/uploads/userphoto/11.jpeg" },
+    { type: "item", title: "Sam Basu", picture: "http://developer.telerik.com/wp-content/uploads/userphoto/161.jpeg" }
+
 ];
 var bindingList = new WinJS.Binding.List(array);
 
